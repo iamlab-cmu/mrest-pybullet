5c88c89515ad1db62a8ca5e13044268885365457
Roberto Shu <rshum@cmu.edu>
2021-10-03 02:17:57 -0400

    Created MSL lab environment in pybullet, but not sure how to localize using lidar data

---
+++ scripts/sim_msl_world.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#  Copyright Microdynamic Systems Laboratory 2021
+#
+# @author Roberto Shu <rshum@cmu.edu>
+#
+# @brief Python script to launch a ballbot simulation in pybullet
+#
+
+import pybullet as p
+import pybullet_data
+import pybullet_utils.bullet_client as bc
+import time
+from datetime import datetime
+import numpy as np
+from enum import Enum
+
+from robot.robot_simulator import *
+from robot.definitions import *
+from environments.corner_env import *
+from environments.environments import MSLEnv, MSLEnv2
+
+# Simulation parameters
+LOG_VIDEO = False
+VIDEO_FILE_NAME = "ballbot_grasp"
+
+if __name__ == "__main__":
+    # set pybullet environment
+    robot_simulator = RobotSimulator(
+        startPos=[0, 0, 0.12], startOrientationEuler=[0, 0, 0])
+
+    """ Main Loop """
+    robot_simulator.update_robot_state(BallState.OLC)
+    robot_simulator.ballbot.set_arm_torque_mode()
+
+    env = MSLEnv(startPos=[0.0, 0., 0.], startOrientationEuler=[
+                   0., 0., 0.])
+    robot_simulator.setup_environment(env)
+
+    if LOG_VIDEO:
+        robot_simulator.start_video_log(VIDEO_FILE_NAME)
+
+    while(1):
+        # Read user params
+        robot_simulator.read_user_params()
+        if USE_ROS:
+            robot_simulator.read_ROS_params()
+        robot_simulator.step()
+        p.stepSimulation()
+
+        if USE_ROS:
+            robot_simulator.publish_ros_data()
+
+
+        time.sleep(SIMULATION_TIME_STEP_S)
+
+    if LOG_VIDEO:
+        robot_simulator.stop_video_log()
